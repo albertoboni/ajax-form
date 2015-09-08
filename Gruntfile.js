a2e6cf5a7306f3274d6b8b54d42539947a747b7d
Alberto Boni <betobonix@gmail.com>
2015-09-08 10:21:34 -0300

    grunt file and refacor of source files

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      main: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'lib/<%= pkg.name %>-<%= pkg.version %>.js'
+      },
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'lib/<%= pkg.name %>-<%= pkg.version %>.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task(s).
+  grunt.registerTask('default', ['copy', 'uglify']);
+
+};
